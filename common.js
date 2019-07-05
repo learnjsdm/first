af90833f79c7020c1666413f6a913acce270790d
learnjs@i.ua <learnjs@i.ua>
2019-07-05 16:37:41 +0300

    First step finished

--- common.js
+++ common.js
@@ -1,1 +1,23 @@
-console.log('Test message');
+"use strict";
+
+let monthBudget = prompt('Ваш бюджет на месяц?', '');
+let currentData = prompt('Введите дату в формате YYYY-MM-DD', '');
+
+let appData = {
+    budget: monthBudget,
+    timeData: currentData,
+    expences: {},
+    optionalExpences: {},
+    income: [],
+    saving: false
+};
+let ans1 = prompt('Обязательная статья расходов');
+let ans2 = prompt('Цена');
+let ans3 = prompt('Обязательная статья расходов');
+let ans4 = prompt('Цена');
+
+
+appData.expences.ans1 = ans2;
+appData.expences.ans3 = ans4;
+const response = monthBudget / 30;
+alert(response.toFixed(2));
